795f626b10fa2a7aa7ccaad99ece036fecac6ffe
line32 <efex32@hotmail.com>
2016-02-05 11:08:38 -0800

    json file created and data extracting from there now

--- js/portfolio_old.js
+++ js/portfolio_old.js
@@ -18,6 +18,50 @@ var portfolio = {
 	projects: []
 };
 
+/*portfolio.rawData = [
+	{
+		title: 'Paypal statement email',
+		url: 'http://cbusch.net/work/paypal_p2p-d_received_en.html',
+		description: 'Responsive email for PayPal statements.',
+		icon: 'images/paypal_stmt.jpg',
+		category: 'email'
+	},
+	{
+		title: 'REI digital catalog map',
+		url: 'http://reitrips.com/#&panel1-3',
+		description: 'Integrated JSON data into google maps API and used jQuery and jQuery UI for filtering.',
+		icon: 'images/rei.jpg',
+		category: 'web'
+	},
+	{
+		title: 'REI promotional email',
+		url: 'http://cbusch.net/work/rei/member_tents/membertents.html',
+		description: 'Responsive promotional email for REI.',
+		icon: 'images/rei_tent.jpg',
+		category: 'email'
+	},
+	{
+		title: 'REI refer landing page',
+		url: 'http://cbusch.net/work/rei/refer_friend/refer_a_friend.html',
+		description: 'Responsive landing page with form and validation.',
+		icon: 'images/rei_refer.jpg',
+		category: 'web'
+	},
+	{
+		title: 'Tablespoon promotional email',
+		url: 'http://cbusch.net/work/tbsp/0811_newsletter.html',
+		description: 'Responsive promotional email for Tablespoon.',
+		icon: 'images/tbsp.jpg',
+		category: 'email'
+	},
+	{
+		title: 'Gamestop RFP',
+		url: 'http://cbusch.net/work/gamestop/gamestop_rfp.html',
+		description: 'Basic site template for RFP',
+		icon: 'images/gamestop.gif',
+		category: 'web'
+	}
+];*/
 function Project(title, url, description, icon, category) {
 	this.title = title;
 	this.url = url;
@@ -70,3 +114,17 @@ $.getJSON("js/projects.json").done(function(data) {
 	portfolioView.populateFilters();
 	portfolioView.handleCategoryFilter();
 });
+//Load raw data into post array
+/*$.each(portfolio.rawData, function() {
+	temp_project = new Project(this.title, this.url, this.description, this.icon, this.category);
+	portfolio.projects.push(temp_project);	
+});
+
+
+//Add posts to html
+
+
+$(document).ready(function() {
+	portfolioView.populateFilters();
+	portfolioView.handleCategoryFilter();
+});*/
---
+++ js/projects.json
@@ -0,0 +1,45 @@
+{
+	"rawData":[
+	{
+		"title": "Paypal statement email",
+		"url": "http://cbusch.net/work/paypal_p2p-d_received_en.html",
+		"description": "Responsive email for PayPal statements.",
+		"icon": "images/paypal_stmt.jpg",
+		"category": "email"
+	},
+	{
+		"title": "REI digital catalog map",
+		"url": "http://reitrips.com/#&panel1-3",
+		"description": "Integrated JSON data into google maps API and used jQuery and jQuery UI for filtering.",
+		"icon": "images/rei.jpg",
+		"category": "web"
+	},
+	{
+		"title": "REI promotional email",
+		"url": "http://cbusch.net/work/rei/member_tents/membertents.html",
+		"description": "Responsive promotional email for REI.",
+		"icon": "images/rei_tent.jpg",
+		"category": "email"
+	},
+	{
+		"title": "REI refer landing page",
+		"url": "http://cbusch.net/work/rei/refer_friend/refer_a_friend.html",
+		"description": "Responsive landing page with form and validation.",
+		"icon": "images/rei_refer.jpg",
+		"category": "web"
+	},
+	{
+		"title": "Tablespoon promotional email",
+		"url": "http://cbusch.net/work/tbsp/0811_newsletter.html",
+		"description": "Responsive promotional email for Tablespoon.",
+		"icon": "images/tbsp.jpg",
+		"category": "email"
+	},
+	{
+		"title": "Gamestop RFP",
+		"url": "http://cbusch.net/work/gamestop/gamestop_rfp.html",
+		"description": "Basic site template for RFP",
+		"icon": "images/gamestop.gif",
+		"category": "web"
+	}
+]}
